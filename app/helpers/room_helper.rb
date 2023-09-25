dc731d7031ff990980243e1903311b73039a3872
ahad <muhammadahad6785@gmail.com>
2023-09-26 03:47:16 +0500

    show user who booked the room

--- app/helpers/room_helper.rb
+++ app/helpers/room_helper.rb
@@ -2,4 +2,14 @@ module RoomHelper
     def room_names
        Room.all.count > 0 ?  Room.all.map { |room| [room.name, room.id] } : []
     end
+
+    def booked_by_user(slot, date, room)
+        found = BookedSlot.find_by(slot_id: slot.id, on_date: date, room_id: room)
+        found ? found.email : '-'
+    end
+
+    def if_already_booked(slot)
+       slot.booked_slot ? true : false
+    # true
+    end
 end
--- app/models/slot.rb
+++ app/models/slot.rb
@@ -2,5 +2,5 @@ class Slot < ApplicationRecord
   attr_accessor :slot_id, :name, :email
 
   belongs_to :room
-  has_one :booked_slot
+  has_one :booked_slot , dependent: :destroy
 end
